5a4c5840649fa7bd9fdb02ab0216925062bc6867
Vishnu Choudhary <31178173+vc7184@users.noreply.github.com>
2018-08-03 14:21:42 +0530

    Update replaceSubStr.java

--- replaceSubStr.java
+++ replaceSubStr.java
@@ -4,12 +4,12 @@ class a
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		String sub = sc.nextLine();
+		String str = sc.nextLine();//Input String
+		String sub = sc.nextLine();//Sub-String
 		int n = sub.length();
 		for(int i=0;i<=str.length()-n;i++){
 			if((str.substring(i,i+n).compareToIgnoreCase(sub))==0){
-				String newStr = sc.nextLine();
+				String newStr = sc.nextLine();//New Sub-String that will replace the old one
 				System.out.println(str.replaceAll(str.substring(i,i+n),newStr));
 			}
 		}
